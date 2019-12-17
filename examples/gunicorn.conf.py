7b604419cc300596c3b06cb97042c124a84abd7c
Serafeim Papastefanos <spapas@gmail.com>
2019-12-17 18:53:04 +0200

    Update gunicorn.conf.py

--- examples/gunicorn.conf.py
+++ examples/gunicorn.conf.py
@@ -4,7 +4,7 @@ _collector_addr = 'tcp://127.0.0.2:2345'
 _prefix = b'my_app'
 
 def pre_request(req, worker):
-    start_request(req, collect=True, collector=_collector_addr, prefix=_prefix)
+    start_request(req, collect=True, collector_addr=_collector_addr, prefix=_prefix)
 
 def post_request(req, worker):
-    end_request(req, collector=_collector_addr, prefix=_prefix)
+    end_request(req, collector_addr=_collector_addr, prefix=_prefix)
